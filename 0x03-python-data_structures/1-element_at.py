49a62f71bf5da176badfafbffb8e22ec8c5d6fd8
JacJohn <jantjiesdenver41@gmail.com>
2022-08-29 06:08:18 -0700

    1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
+
+
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif idx >= len(my_list):
+
+    if idx > (len(my_list) - 1):
         return None
-    else:
-        return my_list[idx]
 
-
-if __name__ == "__main__":
-        element_at(my_list, idx)
+    return (my_list[idx])
