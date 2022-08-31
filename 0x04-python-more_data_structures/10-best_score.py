5d279deda1bc4b297210fe92152ec75007f202bf
JacJohn <jantjiesdenver41@gmail.com>
2022-08-31 08:44:11 -0700

    10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if a_dictionary:
+        max_value = max(a_dictionary.values())
+        for key in a_dictionary.keys():
+            if a_dictionary[key] == max_value:
+                return key
+            else:
+                continue
+    else:
         return None
-    return max(a_dictionary)
+
+
+if __name__ == "__main__":
+    best_score(a_dictionary)
